28ca11d8582ebe946dd7c3fd0206a6dac52a0ada
zakk125 <vannzakk@gmail.com>
2026-05-29 13:16:36 +0700

    add test auto

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ app = create_app()
 
 with app.app_context():
     db.create_all()
+    print("Database tables created successfully!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
